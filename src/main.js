166264d826b398d397b13b171649e8de02673760
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-11 14:22:30 +0700

    import  library and fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+
+library.add({faPhone})
 
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
