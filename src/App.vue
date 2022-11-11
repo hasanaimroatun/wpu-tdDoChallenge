01162ae9367c20e5f78c074f14e30b4b07680657
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-11 14:05:36 +0700

    import todo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ToDo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDoContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo,
   }
 }
 </script>
@@ -21,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 32px;
 }
 </style>
