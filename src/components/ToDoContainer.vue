1251713cd37726d3e4b81c4b1794b5bc20fa0106
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-11 14:08:50 +0700

    create file and eksport

---
+++ src/components/ToDoContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="title">#todo</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'todo-container'
+    }
+</script>
+
+<style scoped>
+
+</style>
