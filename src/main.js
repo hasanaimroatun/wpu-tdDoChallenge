0c234d244b95a3c1783417bb4d3c7c6d4ff8eff7
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-16 18:43:43 +0700

    import icon and add to library

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
 
-library.add({faPhone})
+library.add({faTrashCan})
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
