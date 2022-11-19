dd498aab865224f8cafd9aa0716100444705d014
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-19 18:14:20 +0700

    delete elements and props

--- src/components/AllContent.vue
+++ src/components/AllContent.vue
@@ -1,28 +1,12 @@
 <template>
     <div>
         All Content
-        <div v-for="todo in todoAll" :key="todo">
-            <div class="form-check d-flex position-relative tList">
-                <input class="form-check-input inputListAll" 
-                    type="checkbox" 
-                    id="flexCheckDefault" 
-                    @click="addDecoration($event)"
-                >
-                <label 
-                    class="form-check-label labelListAll" 
-                    for="flexCheckDefault"
-                >
-                    {{todo.data}}
-                </label>
-            </div>
-        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'All-content', 
-        props: ['todoAll', 'addDecoration']
+        name: 'All-content',
     }
 </script>
 
